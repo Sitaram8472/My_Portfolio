18000fc92a7c397755104261fd19dc43ca250d42
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-08-22 18:16:59 +0530

    update portofolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import NavBarSection from './Components/NavBar/NavBarSection'
 import Home from './Components/Home/Home';
 import About from './Components/AboutSection/About';
 import Project from './Components/ProjectSection/Project';
-import Achievements from './Components/Achievements/Achievement.JSX';
+import Achievements from './Components/AchievementSection/Achievements';
 import Contact from './Components/ContactSection/Contact';
 import Footer from './Components/FooterSection/Footer';
 
